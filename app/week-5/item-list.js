b9a23d1f0a369caf45910ea4660880c9e5b80a00
Danny Horning <danny-horning@hotmail.com>
2024-02-14 11:43:23 -0700

    separated the handlesSortBy into two different functions to handle sorting JSON file

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -7,22 +7,30 @@ import items from './items.json';
 export default function ItemList() {
   const [sortBy, setSortBy] = useState('name');
 
-  function handleSortBy(event) {
-    event.preventDefault();
-    return items.sort((a, b) => {
-      if (sortBy === 'name') {
-        return a.name.localeCompare(b.name);
-      } else if (sortBy === 'category') {
-        return a.category.localeCompare(b.category);
-      }
-    })};
+  function handleSortBy(type) {
+    if (type === 'name') {
+        return () => setSortBy('name');
+        }
+    if (type === 'category') {
+        return () => setSortBy('category'); 
+        }}
 
+    const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === 'category') {
+      return a.category.localeCompare(b.category);
+    }
+    return 0;
+  });
+
+  
   return (
   <div>
-    <button onClick={handleSortBy} value="name" className='bg-slate-700 p-1 rounded-md hover:bg-blue-300 active:bg-slate-800'>Sort by Name</button>
-    <button onClick={handleSortBy} value="category" className='bg-slate-700 p-1 rounded-md hover:bg-blue-300 active:bg-slate-800'>Sort by Category</button>
-    <div className='flex flex-wrap justify-center'>
-      {items.map((item, index) => (
+    <button onClick={handleSortBy('name')} value="name" className='bg-slate-900 p-1 rounded-md hover:bg-blue-300 active:bg-slate-800 text-slate-200'>Sort by Name</button>
+    <button onClick={handleSortBy('category')} value="category" className='bg-slate-900 p-1 rounded-md m-1 hover:bg-blue-300 active:bg-slate-800 text-slate-200'>Sort by Category</button>
+    <div className=' text-slate-200'>
+      {sortedItems.map((item, index) => (
         <Item key={index} name={item.name} quantity={item.quantity} category={item.category} />
       ))}
       </div>
